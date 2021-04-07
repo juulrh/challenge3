185ee263b28253a7d266c4a222c0c1035e10ccd5
juulrh <juulrh@gmail.com>
2021-04-07 22:21:05 +0200

    yea

--- main.js
+++ main.js
@@ -263,84 +263,19 @@ map.loadImage('images/flag.png', function (error, image){
                     closeOnClick: false
                 });
 
-            //     map.on('mouseenter', 'places', function (e) {
-            //
-            //         const openTripMapKey = '5ae2e3f221c38a28845f05b65370e244b3b310f07de647889ddf591c';
-            //         let url = 'https://api.opentripmap.com/0.1/en/places/xid/' + e.features[0].id,
-            //             qString = '?apikey=' + openTripMapKey;
-            //         fetch(url + qString)
-            //             .then(resp => {
-            //                 return resp.json();
-            //             }).then(data => {
-            //               let address = '<p>' + data.address.road + ' ' + data.address.house_number + '<br>' + data.address.postcode + ' ' + data.address.city + '<br>' + data.address.country + '</p>';
-            //               var coordinates = e.features[0].geometry.coordinates.slice();
-            //               var description = e.features[0].properties.description + address;
-            //
-            //
-            //             // Populate the popup and set its coordinates based on the feature found.
-            //             popup.setLngLat(coordinates)
-            //                 .setHTML(description)
-            //                 .addTo(map);
-            //         }).catch((error) => {
-            //             alert(error);
-            //         })
-            //     });
-            //
-            //     map.on('mouseleave', 'places', function () {
-            //         popup.remove();
-            //     });
-            // }
-            //
-            //
-            //
-            //
-            //
-            // map.on('load', function () {
-            // 	// Listen for the `geocoder.input` event that is triggered when a user
-            // 	// makes a selection
-            // 	geocoder.on('result', function (ev) {
-            // 	  console.log(ev.result.center);
-            //     //document.getElementById('coordinaten').innerHTML = ev.result.center[0] + '-' + ev.result.center[1];
-            //     getAPIdata(ev.result.center[0], ev.result.center[1]);
-            // 	});
-            // });
-            //
-            //
-            // function getAPIdata(ingevoerdeLon, ingevoerdeLat) {
-            //
-            // 	var request = 'https://api.openweathermap.org/data/2.5/weather?appid=639b70cdea4ec366f54e164e3bc7269c&lon=' +ingevoerdeLon+ '&lat=' +ingevoerdeLat;
-            //
-            // 	fetch(request)  //fetch is geef mij info, vraag stellen aan weathermap
-            //
-            // 	// parse response to JSON format . daarna gebeurt dit,
-            // 	.then(function(response) {
-            // 		return response.json(); //maak van respond een json
-            // 	})
-            //
-            // 	// iets doen met  response
-            // 	.then(function(response) { //hiertussne opschrijven wat ik wil doen met info weather app
-            // 		// show full JSON object
-            // 		console.log(response);//response.main.temp --komt het in de console.
-            // 		var weatherBox = document.getElementById('weer');
-            //
-            // 		weatherBox.innerHTML = 'Weather' + '</br>' + (response.main.temp - 273.15).toFixed(1) + ' &#176;C </br>' + '' + (response.weather[0].description) + '</br>' + 'Windspeed: ' + response.wind.speed + ' m/s';
-            // 	});
-            // }
-            //
-            //
+                map.on('mouseenter', 'places', function (e) {
 
-            map.on('mouseenter', 'places', function (e) {
-
-                    const openTripMapKey = '5ae2e3f221c38a28845f05b65370e244b3b310f07de647889ddf591c';
+                    const openTripMapKey = '5ae2e3f221c38a28845f05b6209bf7bfd923b79af11437d9379c8539';
                     let url = 'https://api.opentripmap.com/0.1/en/places/xid/' + e.features[0].id,
                         qString = '?apikey=' + openTripMapKey;
                     fetch(url + qString)
                         .then(resp => {
                             return resp.json();
                         }).then(data => {
-                        let address = '<p>' + data.address.road + ' ' + data.address.house_number + '<br>' + data.address.postcode + ' ' + data.address.city + '<br>' + data.address.country + '</p>';
-                        var coordinates = e.features[0].geometry.coordinates.slice();
-                        var description = e.features[0].properties.description + address;
+                          let address = '<p>' + data.address.road + ' ' + data.address.house_number + '<br>' + data.address.postcode + ' ' + data.address.city + '<br>' + data.address.country + '</p>';
+                          var coordinates = e.features[0].geometry.coordinates.slice();
+                          var description = e.features[0].properties.description + address;
+
 
                         // Populate the popup and set its coordinates based on the feature found.
                         popup.setLngLat(coordinates)
@@ -355,6 +290,45 @@ map.loadImage('images/flag.png', function (error, image){
                     popup.remove();
                 });
             }
+
+
+
+
+
+            map.on('load', function () {
+            	// Listen for the `geocoder.input` event that is triggered when a user
+            	// makes a selection
+            	geocoder.on('result', function (ev) {
+            	  console.log(ev.result.center);
+                //document.getElementById('coordinaten').innerHTML = ev.result.center[0] + '-' + ev.result.center[1];
+                getAPIdata(ev.result.center[0], ev.result.center[1]);
+            	});
+            });
+
+
+            function getAPIdata(ingevoerdeLon, ingevoerdeLat) {
+
+            	var request = 'https://api.openweathermap.org/data/2.5/weather?appid=639b70cdea4ec366f54e164e3bc7269c&lon=' +ingevoerdeLon+ '&lat=' +ingevoerdeLat;
+
+            	fetch(request)  //fetch is geef mij info, vraag stellen aan weathermap
+
+            	// parse response to JSON format . daarna gebeurt dit,
+            	.then(function(response) {
+            		return response.json(); //maak van respond een json
+            	})
+
+            	// iets doen met  response
+            	.then(function(response) { //hiertussne opschrijven wat ik wil doen met info weather app
+            		// show full JSON object
+            		console.log(response);//response.main.temp --komt het in de console.
+            		var weatherBox = document.getElementById('weer');
+
+            		weatherBox.innerHTML = 'Weather' + '</br>' + (response.main.temp - 273.15).toFixed(1) + ' &#176;C </br>' + '' + (response.weather[0].description) + '</br>' + 'Windspeed: ' + response.wind.speed + ' m/s';
+            	});
+            }
+
+
+
 
 //WERKT WEL, GEKKE DATUM EN TEKST
  // function getNews() {
